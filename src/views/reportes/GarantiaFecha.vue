6fc6b01694e7e3eaa2fadcc838805f073793616c
Juan M. Mulki A <jmulki@sooft.com.ar>
2021-04-16 11:58:34 -0300

    Layout de reporte garantia por fecha

--- src/views/reportes/GarantiaFecha.vue
+++ src/views/reportes/GarantiaFecha.vue
@@ -1,9 +1,117 @@
 <template>
-  <div><h4>Reportes</h4></div>
+  <div>
+    <h4>
+      Reporte <b-icon icon="arrow-right-short"></b-icon> Vencimientos de
+      Garantía por Fecha
+    </h4>
+    <br />
+
+    <b-card>
+      <b-row>
+        <b-col cols="12" sm="12" md="6" lg="3" xl="3">
+          <b-form-group
+            label="Fecha:"
+            description="Puedes cambiarla para visualizar alertas anticipadas."
+          >
+            <b-form-input type="date" v-model="fecha" required></b-form-input>
+          </b-form-group>
+        </b-col>
+        <b-col cols="12" sm="12" md="6" lg="9" xl="9">
+          <div class="botonera">
+            <b-button @click="print" variant="info"
+              ><b-icon icon="search"></b-icon>&nbsp;Consultar</b-button
+            >&nbsp;
+            <b-button @click="print" variant="success"
+              ><b-icon-file-text></b-icon-file-text>&nbsp;Imprimir</b-button
+            >
+          </div>
+        </b-col>
+      </b-row>
+    </b-card>
+
+    <b-card>
+      <div id="printMe">
+        <div class="tituloReporte">
+          ALBIERO SISTEMAS - Mantenimiento de Flota
+          <br />Reporte :: Mi Flota
+        </div>
+        <b-table :items="items" :fields="fields" responsive> </b-table>
+      </div>
+    </b-card>
+  </div>
 </template>
 
 <script>
-export default {};
+import Data from "../../data/data";
+import moment from "moment";
+
+export default {
+  name: "Flota",
+  mixins: [Data],
+  data() {
+    return {
+      endpoint: "vehiculo",
+      fecha: null,
+      items: null,
+      fields: [
+        {
+          key: "id",
+          label: "#",
+        },
+        {
+          key: "tipo.tipo",
+          label: "Tipo",
+        },
+        {
+          key: "descripcion",
+          label: "Descripción",
+        },
+        {
+          key: "alias",
+          label: "Alias",
+        },
+        {
+          key: "patente",
+          label: "Patente",
+        },
+        {
+          key: "anio",
+          label: "Modelo",
+        },
+        {
+          key: "fecha_venc_gtia",
+          label: "Venc. Gtía.",
+          formatter: "dateFormat",
+        },
+        {
+          key: "chofer.apenom",
+          label: "Chofer",
+        },
+      ],
+    };
+  },
+  methods: {
+    dateFormat(value) {
+      return moment(value).format("DD/MM/YYYY");
+    },
+    print() {
+      this.$htmlToPaper("printMe");
+    },
+  },
+  created() {
+    this.fecha = moment().format("YYYY-MM-DD");
+    this.getData(this.endpoint, null).then((response) => {
+      this.items = response;
+    });
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.botonera {
+  margin-top: 30px;
+}
+.tituloReporte {
+  display: none;
+}
+</style>
